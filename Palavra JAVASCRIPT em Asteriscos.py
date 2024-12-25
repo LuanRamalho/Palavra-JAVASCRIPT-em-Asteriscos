787ea0bdeb5c11d5e6414af9602ee51b146b69c5
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2024-12-25 13:48:05 -0300

    Add files via upload

---
+++ Palavra JAVASCRIPT em Asteriscos.py
@@ -0,0 +1,13 @@
+# Programa para imprimir a palavra JAVASCRIPT com asteriscos
+
+def imprimir_com_asteriscos():
+    # Cada letra da palavra "JAVASCRIPT" representada por asteriscos
+    print("   ***      *      *   *      *       ****     ****    ****      ***     ****     ***** ")
+    print("    *      * *     *   *     * *     *        *        *   *      *      *   *      *   ")
+    print("    *     *****    *   *    *****     ***     *        ****       *      ****       *   ")
+    print(" *  *     *   *     * *     *   *        *    *        *  *       *      *          *   ")
+    print("  **      *   *      *      *   *    ****      ****    *   *     ***     *          *   ")
+
+# Chamada da função para exibir o resultado
+imprimir_com_asteriscos()
+input()
